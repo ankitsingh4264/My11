7723d514952bb189ef835cb5b58d725ba14690a1
iamrv-sachan <rajeevsachan1999@gmail.com>
2021-03-22 18:09:33 +0530

    Future match and live match with time in IST

--- app/src/main/java/com/example/my11/Matches/LiveMatchAdapter.kt
+++ app/src/main/java/com/example/my11/Matches/LiveMatchAdapter.kt
@@ -8,6 +8,10 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.my11.DataClass.Matche
 import com.example.my11.R
 import kotlinx.android.synthetic.main.list_of_future_match.view.*
+import java.text.ParseException
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.collections.ArrayList
 
 class LiveMatchAdapter(val list: ArrayList<Matche>) : RecyclerView.Adapter<viewholder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): viewholder {
@@ -29,17 +33,40 @@ class viewholder(itemView: View) : RecyclerView.ViewHolder(itemView){
         with(itemView){
             team1.text=data.team1
             team2.text=data.team2
-            date_text_view.text=getdate(data.dateTimeGMT)
-            time_text_view.text=gettime(data.dateTimeGMT)
+            val dateString = getfulldateinUTC(data.dateTimeGMT)
+//            val date = dateString.getDateWithServerTimeStamp()
+//            val x=getdate(data.dateTimeGMT)
+            date_text_view.text= getdate(data.dateTimeGMT)
+            time_text_view.text=gettime(dateString!!.getDateWithServerTimeStamp().toString())
         }
 
     }
 
-    private fun gettime(dateTimeGMT: String): CharSequence? {
-        return dateTimeGMT.substring(11)
+    fun String.getDateWithServerTimeStamp(): Date? {
+        val dateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'",
+                Locale.getDefault())
+        dateFormat.timeZone = TimeZone.getTimeZone("GMT")  // IMP !!!
+        try {
+            return dateFormat.parse(this)
+        } catch (e: ParseException) {
+            return null
+        }
+    }
+//    /** OUTPUT **/
+//    String To Date Conversion Tue Jan 09 15:06:41 GMT+08:00 2018
+//    Date To String Conversion 2018-01-09T07:06:41.747Z
+//
+
+
+    private fun gettime(dateTimeGMT: String): String? {
+        return dateTimeGMT.substring(11,20)
     }
 
-    private fun getdate(dateTimeGMT: String): CharSequence? {
+    private fun getdate(dateTimeGMT: String): String? {
         return dateTimeGMT.substring(0,10)
+    }
+
+    private fun getfulldateinUTC(dateTimeGMT: String): String? {
+        return dateTimeGMT.substring(0)
     }
 }
