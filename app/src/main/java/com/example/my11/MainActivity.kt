e111f5c0939202519476f449f6626319c687e4dc
ankitsingh4264 <singh.ankit2117@gmail.com>
2021-03-22 16:08:13 +0530

    implemented bottom nav

--- app/src/main/java/com/example/my11/MainActivity.kt
+++ app/src/main/java/com/example/my11/MainActivity.kt
@@ -3,8 +3,11 @@ package com.example.my11
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.my11.API.CricService
 import com.example.my11.API.RetrofitInstance
+import kotlinx.android.synthetic.main.activity_main.*
 import retrofit2.Call
 import retrofit2.Response
 import javax.security.auth.callback.Callback
@@ -14,22 +17,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        getMatch()
+
+        val navController=findNavController(R.id.hostfragment);
+        bottomNav.setupWithNavController(navController);
+
 
     }
 
-    private fun getMatch() {
-         val res=RetrofitInstance.cricInstance.matchCalender(1)
-        res.enqueue(object : retrofit2.Callback<Match>{
-            override fun onResponse(call: Call<Match>, response: Response<Match>) {
-                  val result=response.body()
-
-                    Log.i("ankit",response.toString())
-            }
-
-            override fun onFailure(call: Call<Match>, t: Throwable) {
-                Log.i("ankit","${t.message}")
-            }
-        })
-    }
+//    private fun getMatch() {
+//         val res=RetrofitInstance.cricInstance.matchCalender(1)
+//        res.enqueue(object : retrofit2.Callback<Match>{
+//            override fun onResponse(call: Call<Match>, response: Response<Match>) {
+//                  val result=response.body()
+//
+//                    Log.i("ankit",response.toString())
+//            }
+//
+//            override fun onFailure(call: Call<Match>, t: Throwable) {
+//                Log.i("ankit","${t.message}")
+//            }
+//        })
+//    }
 }
